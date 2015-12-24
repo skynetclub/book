5dc65e6d6a9fc7b62330f9a8f917f23727436041
forthxu <1072353388@qq.com>
2015-12-24 10:41:08 +0800

    update 转移项目到skynetclub

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # git submodule init
 # git submodule update #skynetwiki/FAQ.md 格式有问题
-rm -rf ../talkbox_gh-pages
+rm -rf ../book_gh-pages
 
 gitbook init
-gitbook build --output=../talkbox_gh-pages
+gitbook build --output=../book_gh-pages
 
 DATA_SECOND=`date +%Y-%m-%d-%H-%M-%S`
-cd ../talkbox_gh-pages
+cd ../book_gh-pages
 rm -rf ./skynetwiki/.git
 rm -rf ./build.sh
 git init
 git add .
 git commit -a -m "update ${DATA_SECOND}"
-git remote add origin git@github.com:forthxu/talkbox.git
+git remote add origin git@github.com:skynetclub/book.git
 git push --force origin master:gh-pages
 cd -
